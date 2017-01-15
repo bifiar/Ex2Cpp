22a897305b4e5dcf156ed3af113dffa1d4115101
AvishayAss <Avishay.ass@gmail.com>
2017-01-15 19:58:41 +0200

    Creating cycle Genric LInkedList TODO: implement Merge Option on BestFit

---
+++ double_linked.h
@@ -0,0 +1,127 @@
+//
+// Created by avishay on 1/15/17.
+//
+
+#ifndef EX2CPP_DOUBLE_LINKED_H
+#define EX2CPP_DOUBLE_LINKED_H
+
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class double_linked
+{
+    struct node
+    {
+        T data;
+        node* prev;
+        node* next;
+        node(T t, node* p, node* n) : data(t), prev(p), next(n) {}
+    };
+    node* head;
+    node* tail;
+    size_t _size;
+public:
+    double_linked() : head( NULL ), tail ( NULL ), _size(0) {}
+    template<int N>
+    double_linked( T (&arr) [N]) : head( NULL ), tail ( NULL )
+    {
+        for( int i(0); i != N; ++i)
+            push_back(arr[i]);
+    }
+
+    size_t get_size() const;
+
+    bool empty() const { return ( !head || !tail ); }
+    operator bool() const { return !empty(); }
+    void push_back(T);
+    void push_front(T);
+    T pop_back();
+    T pop_front();
+
+    ~double_linked()
+    {
+        while(head)
+        {
+            node* temp(head);
+            head=head->next;
+            delete temp;
+        }
+    }
+};
+
+template <typename T>
+void double_linked<T>::push_back(T data)
+{
+    tail = new node(data, tail, NULL);
+    if( tail->prev )
+        tail->prev->next = tail;
+
+    if( empty() )
+        head = tail;
+
+    _size++;
+}
+
+template <typename T>
+void double_linked<T>::push_front(T data)
+{
+    head = new node(data, NULL, head);
+    if( head->next )
+        head->next->prev = head;
+
+    if( empty() )
+        tail = head;
+
+    _size++;
+}
+
+template<typename T>
+T double_linked<T>::pop_back()
+{
+    if( empty() ){
+       return  NULL;
+        //throw("double_linked : list empty");
+        //std::cout<<"double_linked : list empty"<<std::endl;
+    }
+
+    node* temp(tail);
+    T data( tail->data );
+    tail = tail->prev ;
+
+    if( tail )
+        tail->next = NULL;
+    else
+        head = NULL ;
+
+    delete temp;
+    _size--;
+    return data;
+}
+
+template<typename T>
+T double_linked<T>::pop_front()
+{
+    if( empty() )
+        throw("double_linked : list empty");
+    node* temp(head);
+    T data( head->data );
+    head = head->next ;
+
+    if( head )
+        head->prev = NULL;
+    else
+        tail = NULL;
+
+    delete temp;
+    _size--;
+    return data;
+}
+template<typename T>
+size_t double_linked<T>::get_size() const {
+    return _size;
+}
+
+
+#endif //EX2CPP_DOUBLE_LINKED_H
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "sharedTypeDeffs.h"
 #include "BestFIt.h"
 #include "FirstFit.h"
+#include "double_linked.h"
 
 #define byte_size 8
 
@@ -23,50 +24,62 @@ int main() {
 
     mm->set_strategy(firstFit);
 
-    int* arr=new int[10];
-    for (int i = 0; i <10 ; ++i) {
-        arr[i]=pow(2.0,i);
+    int arr[] = { 4, 6, 8, 32, 19 } ;
+    double_linked<int>* dlist = new double_linked<int> ( arr );
+    dlist->push_back( 11 );
+    dlist->push_front( 100 );
+
+    while(dlist->get_size()>0){
+        cout << dlist->pop_back()  << " "<<endl;
     }
 
-   char* mm11=new char('3');char* mm12=new char('A');char* mm13=new char('a');
-    int* mm21=new int(35);int* mm22=new int(454);int* mm23=new int(434);
-    double* mm31=new double(43.43);double* mm32=new double(434.43);double* mm33=new double(4375.5454);
-    cout<<"Boaz Check: "<<std::dec<<mm22<<endl;
 
-
-
-
-    //print allocated map
-    un_mapMem* allocatedMem=(mm->get_allocatedMem());
-
-    ConsolePrinter::printMap(allocatedMem);
-
-    delete mm12;delete mm22;delete mm32;
-
-    cout<<"after deleting.... "<<endl;
-    ConsolePrinter::printMap(allocatedMem);
-    freeMapMem* freeMap=mm->get_freeMap();
-    cout<<"free map:"<<endl;
-    //print freelist map
-    ConsolePrinter::printMapSet(freeMap);
-
-    char* mm7=new char;
-    cout<<"after adding memo from free map(mm3)"<<endl;
-    cout<<"Free MAP"<<endl;
-    ConsolePrinter::printMapSet(freeMap);
-    cout<<"Alloc MAP"<<endl;
-    ConsolePrinter::printMap(allocatedMem);
-    cout<<"Address mm7 is "<<(void*) mm7<<endl;
-
-    *mm11='C';*mm12='H';
-    *mm21=212;*mm22=313;
-    *mm31=4242.42;*mm32=3535.34;
-    arr[8]=121267868;
-    cout<<"after varibels balagan check"<<endl;
-    cout<<"m11,m12 "<<mm11<<" "<<mm12<<endl;
-    cout<<"m21,m22 "<<*mm21<<" "<<*mm22<<endl;
-    cout<<"m31,m32 "<<*mm31<<" "<<*mm32<<endl;
-    for (int j = 0; j <10 ; ++j) {
-        cout<<" "<<arr[j];
-    }
 }
+
+
+//int* arr=new int[10];
+//    for (int i = 0; i <10 ; ++i) {
+//        arr[i]=pow(2.0,i);
+//    }
+//
+//   char* mm11=new char('3');char* mm12=new char('A');char* mm13=new char('a');
+//    int* mm21=new int(35);int* mm22=new int(454);int* mm23=new int(434);
+//    double* mm31=new double(43.43);double* mm32=new double(434.43);double* mm33=new double(4375.5454);
+//    cout<<"Boaz Check: "<<std::dec<<mm22<<endl;
+//
+//
+//
+//
+//    //print allocated map
+//    un_mapMem* allocatedMem=(mm->get_allocatedMem());
+//
+//    ConsolePrinter::printMap(allocatedMem);
+//
+//    delete mm12;delete mm22;delete mm32;
+//
+//    cout<<"after deleting.... "<<endl;
+//    ConsolePrinter::printMap(allocatedMem);
+//    freeMapMem* freeMap=mm->get_freeMap();
+//    cout<<"free map:"<<endl;
+//    //print freelist map
+//    ConsolePrinter::printMapSet(freeMap);
+//
+//    char* mm7=new char;
+//    cout<<"after adding memo from free map(mm3)"<<endl;
+//    cout<<"Free MAP"<<endl;
+//    ConsolePrinter::printMapSet(freeMap);
+//    cout<<"Alloc MAP"<<endl;
+//    ConsolePrinter::printMap(allocatedMem);
+//    cout<<"Address mm7 is "<<(void*) mm7<<endl;
+//
+//    *mm11='C';*mm12='H';
+//    *mm21=212;*mm22=313;
+//    *mm31=4242.42;*mm32=3535.34;
+//    arr[8]=121267868;
+//    cout<<"after varibels balagan check"<<endl;
+//    cout<<"m11,m12 "<<mm11<<" "<<mm12<<endl;
+//    cout<<"m21,m22 "<<*mm21<<" "<<*mm22<<endl;
+//    cout<<"m31,m32 "<<*mm31<<" "<<*mm32<<endl;
+//    for (int j = 0; j <10 ; ++j) {
+//        cout<<" "<<arr[j];
+//    }
